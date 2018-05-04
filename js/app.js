e3bbe1a7c26f94e2ab2ccd830e48372944a87919
bharathparsam <bharathparsam@live.com>
2018-05-04 17:06:09 +0530

    Error in time,stars functionality fixed

--- js/app.js
+++ js/app.js
@@ -286,3 +286,15 @@ document.addEventListener('DOMContentLoaded', _ => {
     }
   });
   //close Document.Ready
+
+
+  /*
+ * set up the event listener for a card. If a card is clicked:   -->Completed
+ *  - display the card's symbol (put this functionality in another function that you call from this one) -->Completed
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) -->Completed
+ *  - if the list already has another card, check to see if the two cards match-->completed
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) -->Completed
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) -->Completed
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one) -->Completed
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one) -->Completed
+ */
